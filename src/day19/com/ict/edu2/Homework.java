05dd75ae50e043b52800facc3f911edeea184b89
parkchaelim <pcl8205@naver.com>
2023-05-23 20:18:30 +0900

    homework upload

--- src/day19/com/ict/edu2/Homework.java
+++ src/day19/com/ict/edu2/Homework.java
@@ -5,7 +5,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Enumeration;
 
+import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,8 +22,8 @@ import javax.swing.ScrollPaneConstants;
 
 // 계산기 만들기(이벤트 처리)
 
-public class Homework extends JFrame implements ActionListener, ItemListener{
-	
+public class Homework extends JFrame implements ActionListener {
+
 	JPanel jp1, jp2;
 	JTextField jtf1, jtf2;
 	JRadioButton jrb1, jrb2, jrb3, jrb4;
@@ -28,95 +31,115 @@ public class Homework extends JFrame implements ActionListener, ItemListener{
 	JTextArea jta;
 	JScrollPane jsp;
 	JButton jb1, jb2, jb3;
-	
+	double su = 0;
+
 	public Homework() {
-		 super("계산기");
-		 
-		 jp1 = new JPanel();
-		 
-		 jtf1 = new JTextField(10);
-		 jtf2 = new JTextField(10);
-		 
-		 jrb1 = new JRadioButton(" + ");
-		 jrb2 = new JRadioButton(" - ");
-		 jrb3 = new JRadioButton(" * ");
-		 jrb4 = new JRadioButton(" / ");
-		 
-		 bg = new ButtonGroup();
-		 
-		 bg.add(jrb1);
-		 bg.add(jrb2);
-		 bg.add(jrb3);
-		 bg.add(jrb4);
-		 
-		 jp1.add(new JLabel("수 1 : "));
-		 jp1.add(jtf1);
-		 jp1.add(new JLabel("수 2 : "));
-		 jp1.add(jtf2);
-		 jp1.add(new JLabel("연산자 : "));
-		 jp1.add(jrb1);
-		 jp1.add(jrb2);
-		 jp1.add(jrb3);
-		 jp1.add(jrb4);
-		 
-		 jta = new JTextArea(20,0);
-		 jta.setEditable(false);
-		 jta.setLineWrap(true);
-		 jsp = new JScrollPane(jta,
-				 ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-				 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
-				 );
-		 
-		 jp2 = new JPanel();
-		 
-		 jb1 = new JButton("계산");
-		 jb2 = new JButton("종료");
-		 jb3 = new JButton("초기화");
-		 
-		 jp2.add(jb1);
-		 jp2.add(jb2);
-		 jp2.add(jb3);
-		 
-		 add(jp1, BorderLayout.NORTH);
-		 add(jsp, BorderLayout.CENTER);
-		 add(jp2, BorderLayout.SOUTH);
-		 
-		 pack();
-		 setLocationRelativeTo(null);
-		 setVisible(true);
-		 setDefaultCloseOperation(EXIT_ON_CLOSE);
-		 
-		 jtf1.addActionListener(this);
-		 jtf2.addActionListener(this);
-		
-		 jrb1.addItemListener(this);
-		 jrb2.addItemListener(this);
-		 jrb3.addItemListener(this);
-		 jrb4.addItemListener(this);
-		
-		 jb1.addActionListener(this);
-		 jb2.addActionListener(this);
-		 jb3.addActionListener(this);
+		super("계산기");
+
+		jp1 = new JPanel();
+
+		jtf1 = new JTextField(10);
+		jtf2 = new JTextField(10);
+
+		jrb1 = new JRadioButton(" + ");
+		jrb2 = new JRadioButton(" - ");
+		jrb3 = new JRadioButton(" * ");
+		jrb4 = new JRadioButton(" / ");
+
+		bg = new ButtonGroup();
+
+		bg.add(jrb1);
+		bg.add(jrb2);
+		bg.add(jrb3);
+		bg.add(jrb4);
+
+		jp1.add(new JLabel("수 1 : "));
+		jp1.add(jtf1);
+		jp1.add(new JLabel("수 2 : "));
+		jp1.add(jtf2);
+		jp1.add(new JLabel("연산자 : "));
+		jp1.add(jrb1);
+		jp1.add(jrb2);
+		jp1.add(jrb3);
+		jp1.add(jrb4);
+
+		jta = new JTextArea(20, 0);
+		jta.setEditable(false);
+		jta.setLineWrap(true);
+		jsp = new JScrollPane(jta, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+		jp2 = new JPanel();
+
+		jb1 = new JButton("계산");
+		jb2 = new JButton("종료");
+		jb3 = new JButton("초기화");
+
+		jp2.add(jb1);
+		jp2.add(jb2);
+		jp2.add(jb3);
+
+		add(jp1, BorderLayout.NORTH);
+		add(jsp, BorderLayout.CENTER);
+		add(jp2, BorderLayout.SOUTH);
+
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		jb1.addActionListener(this);
+		jb2.addActionListener(this);
+		jb3.addActionListener(this);
+
+		jrb1.addActionListener(this);
+		jrb2.addActionListener(this);
+		jrb3.addActionListener(this);
+		jrb4.addActionListener(this);
 	}
-	
-	@Override
-	public void itemStateChanged(ItemEvent e) {
-		JRadioButton obj = (JRadioButton)e.getSource();
-		if (e.getStateChange() == e.SELECTED) {
-			jta.append(obj.);
-		}else if(e.getStateChange() == e.DESELECTED) {
-			jta.append(obj.);
-		}
-	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		double d1, d2;
+		JButton obj = (JButton) e.getSource();
+
+		if (obj == jb2) {
+			System.exit(0);
+		} else if (obj == jb3) {
+			bg.clearSelection();
+			jtf1.setText("");
+			jtf2.setText("");
+			jta.setText("");
+		}
+
+		Enumeration<AbstractButton> list = bg.getElements();
+		while (list.hasMoreElements()) {
+			d1 = Double.parseDouble(jtf1.getText());
+			d2 = Double.parseDouble(jtf2.getText());
+			AbstractButton abstractButton = (AbstractButton) list.nextElement();
+			JRadioButton jb = (JRadioButton) abstractButton;
+			if (jb.isSelected()) {
+				if (obj == jb1) {
+					if (jb == jrb1) {
+						su = d1 + d2;
+						jta.setText((int) d1 + " + " + (int) d2 + " = " + (int) su);
+					} else if (jb == jrb2) {
+						su = d1 - d2;
+						jta.setText((int) d1 + " - " + (int) d2 + " = " + (int) su);
+					} else if (jb == jrb3) {
+						su = d1 * d2;
+						jta.setText((int) d1 + " * " + (int) d2 + " = " + (int) su);
+					} else if (jb == jrb4) {
+						su = d1 / d2;
+						jta.setText((int) d1 + " / " + (int) d2 + " = " + (int) (su * 10) / 10.0);
+					}
+
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-	new Homework();
-}
-
+		new Homework();
+	}
 }
--- src/day19/com/ict/edu2/Homework_grade.java
+++ src/day19/com/ict/edu2/Homework_grade.java
@@ -1,5 +1,127 @@
 package day19.com.ict.edu2;
 
-public class Homework_grade {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+public class Homework_grade extends JFrame implements ActionListener {
+	JPanel jp1, jp2, jp3, sp1;
+	JTextField name, kor, eng, math;
+	JTextArea jta;
+	JScrollPane jsp;
+	JButton jb1, jb2, jb3;
+
+	public Homework_grade() {
+		super("성적");
+
+		jp1 = new JPanel();
+		name = new JTextField(10);
+
+		jp2 = new JPanel();
+		kor = new JTextField(10);
+		eng = new JTextField(10);
+		math = new JTextField(10);
+
+		jp1.add(new JLabel("이름 : "));
+		jp1.add(name);
+		jp2.add(new JLabel("국어 : "));
+		jp2.add(kor);
+		jp2.add(new JLabel("영어 : "));
+		jp2.add(eng);
+		jp2.add(new JLabel("수학 : "));
+		jp2.add(math);
+
+		sp1 = new JPanel(new GridLayout(2, 1));
+		sp1.add(jp1);
+		sp1.add(jp2);
+
+		jta = new JTextArea(20, 0);
+		jta.setEditable(false);
+		jta.setLineWrap(true);
+		jsp = new JScrollPane(jta, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+		jp3 = new JPanel();
+		jb1 = new JButton("계산");
+		jb2 = new JButton("종료");
+		jb3 = new JButton("초기화");
+
+		jp3.add(jb1);
+		jp3.add(jb2);
+		jp3.add(jb3);
+
+		add(sp1, BorderLayout.NORTH);
+		add(jsp, BorderLayout.CENTER);
+		add(jp3, BorderLayout.SOUTH);
+
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		jb1.addActionListener(this);
+		jb2.addActionListener(this);
+		jb3.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton obj = (JButton) e.getSource();
+	
+		if (obj == jb2) {
+			System.exit(0);
+		} else if (obj == jb3) {
+			name.setText("");
+			kor.setText("");
+			eng.setText("");
+			math.setText("");
+			jta.setText("");
+		}
+	
+		String name;
+		int sum;
+		String hak;
+		double avg;
+		int su1 = 0, su2 = 0, su3 = 0;
+	
+		if (obj == jb1) {
+			su1 = Integer.parseInt(kor.getText());
+			su2 = Integer.parseInt(eng.getText());
+			su3 = Integer.parseInt(math.getText());
+		
+			name = this.name.getText();
+			sum = su1 + su2 + su3;
+			avg = (int) (sum / 3.0 * 10) / 10.0;
+			if (avg >= 90) {
+				hak = "A학점";
+			} else if (avg >= 80) {
+				hak = "B학점";
+			} else if (avg >= 70) {
+				hak = "C학점";
+			} else {
+				hak = "F학점";
+			}
+			jta.setText("이름 : " + name + "\n"
+			+ "총점 : " + sum + "\n" 
+			+ "평균 : " + avg + "\n" 
+			+ "학점 : " + hak);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Homework_grade();
+	}
 }
