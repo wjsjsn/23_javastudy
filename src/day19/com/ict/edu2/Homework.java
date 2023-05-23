63a26a3fd36b72e64b04aaa5cbaa7f4c95f74fe7
parkchaelim <pcl8205@naver.com>
2023-05-23 17:23:47 +0900

    세번째

--- src/day19/com/ict/edu2/Homework.java
+++ src/day19/com/ict/edu2/Homework.java
@@ -1,6 +1,122 @@
 package day19.com.ict.edu2;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
 // 계산기 만들기(이벤트 처리)
 
-public class Homework {
- 
+public class Homework extends JFrame implements ActionListener, ItemListener{
+	
+	JPanel jp1, jp2;
+	JTextField jtf1, jtf2;
+	JRadioButton jrb1, jrb2, jrb3, jrb4;
+	ButtonGroup bg;
+	JTextArea jta;
+	JScrollPane jsp;
+	JButton jb1, jb2, jb3;
+	
+	public Homework() {
+		 super("계산기");
+		 
+		 jp1 = new JPanel();
+		 
+		 jtf1 = new JTextField(10);
+		 jtf2 = new JTextField(10);
+		 
+		 jrb1 = new JRadioButton(" + ");
+		 jrb2 = new JRadioButton(" - ");
+		 jrb3 = new JRadioButton(" * ");
+		 jrb4 = new JRadioButton(" / ");
+		 
+		 bg = new ButtonGroup();
+		 
+		 bg.add(jrb1);
+		 bg.add(jrb2);
+		 bg.add(jrb3);
+		 bg.add(jrb4);
+		 
+		 jp1.add(new JLabel("수 1 : "));
+		 jp1.add(jtf1);
+		 jp1.add(new JLabel("수 2 : "));
+		 jp1.add(jtf2);
+		 jp1.add(new JLabel("연산자 : "));
+		 jp1.add(jrb1);
+		 jp1.add(jrb2);
+		 jp1.add(jrb3);
+		 jp1.add(jrb4);
+		 
+		 jta = new JTextArea(20,0);
+		 jta.setEditable(false);
+		 jta.setLineWrap(true);
+		 jsp = new JScrollPane(jta,
+				 ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+				 );
+		 
+		 jp2 = new JPanel();
+		 
+		 jb1 = new JButton("계산");
+		 jb2 = new JButton("종료");
+		 jb3 = new JButton("초기화");
+		 
+		 jp2.add(jb1);
+		 jp2.add(jb2);
+		 jp2.add(jb3);
+		 
+		 add(jp1, BorderLayout.NORTH);
+		 add(jsp, BorderLayout.CENTER);
+		 add(jp2, BorderLayout.SOUTH);
+		 
+		 pack();
+		 setLocationRelativeTo(null);
+		 setVisible(true);
+		 setDefaultCloseOperation(EXIT_ON_CLOSE);
+		 
+		 jtf1.addActionListener(this);
+		 jtf2.addActionListener(this);
+		
+		 jrb1.addItemListener(this);
+		 jrb2.addItemListener(this);
+		 jrb3.addItemListener(this);
+		 jrb4.addItemListener(this);
+		
+		 jb1.addActionListener(this);
+		 jb2.addActionListener(this);
+		 jb3.addActionListener(this);
+	}
+	
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		JRadioButton obj = (JRadioButton)e.getSource();
+		if (e.getStateChange() == e.SELECTED) {
+			jta.append(obj.);
+		}else if(e.getStateChange() == e.DESELECTED) {
+			jta.append(obj.);
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+	new Homework();
+}
+
 }
--- src/day20/com/ict/edu3/Ex01_Menu.java
+++ src/day20/com/ict/edu3/Ex01_Menu.java
@@ -2,7 +2,13 @@ package day20.com.ict.edu3;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Enumeration;
 
+import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -12,7 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-public class Ex01_Menu extends JFrame {
+public class Ex01_Menu extends JFrame implements ActionListener, ItemListener {
 	JPanel jp1, jp2, jp3, jp4, jp5;
 	JButton jb1, jb2;
 	JTextField jtf;
@@ -106,9 +112,91 @@ public class Ex01_Menu extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		jb1.addActionListener(this);
+		jb2.addActionListener(this);
+
+		jrb1.addItemListener(this);
+		jrb2.addItemListener(this);
+		jrb3.addItemListener(this);
+		jrb4.addItemListener(this);
+		jrb5.addItemListener(this);
+		jrb6.addItemListener(this);
+		jrb7.addItemListener(this);
+		jrb8.addItemListener(this);
+		jrb9.addItemListener(this);
+		jrb10.addItemListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton obj = (JButton) e.getSource();
+		if (obj == jb1) {
+			jtf.setText((int) (price) + "원");
+		} else if (obj == jb2) {
+			jtf.setText("");
+			jrb1.setSelected(true);
+			jrb4.setSelected(true);
+			jrb9.setSelected(true);
+		}
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// bg1에 존재하는 요소들을 배열로 만듦
+		Enumeration<AbstractButton> bg1_list = bg1.getElements();
+		while (bg1_list.hasMoreElements()) {
+			// bg1에 있는 요소를 꺼내서 JRadioButton으로 형변환
+			AbstractButton abstractButton = (AbstractButton) bg1_list.nextElement();
+			JRadioButton jb = (JRadioButton) abstractButton;
+			if (jb.isSelected()) {
+				if (jb == jrb1) {
+					price = 15000;
+				} else if (jb == jrb2) {
+					price = 20000;
+				} else if (jb == jrb3) {
+					price = 30000;
+				}
+			}
+		}
+		// bg2에 존재하는 요소들을 배열로 만듦
+		Enumeration<AbstractButton> bg2_list = bg2.getElements();
+		while (bg2_list.hasMoreElements()) {
+			// bg2에 있는 요소를 꺼내서 JRadioButton으로 형변환
+			AbstractButton abstractButton = (AbstractButton) bg2_list.nextElement();
+			JRadioButton jb = (JRadioButton) abstractButton;
+			if (jb.isSelected()) {
+				if (jb == jrb4) {
+					price = price + 500;
+				} else if (jb == jrb5) {
+					price = price + 1000;
+				} else if (jb == jrb6) {
+					price = price + 1500;
+				} else if (jb == jrb7) {
+					price = price + 2000;
+				}
+			}
+		}
+		// bg3에 존재하는 요소들을 배열로 만듦
+		Enumeration<AbstractButton> bg3_list = bg3.getElements();
+		while (bg3_list.hasMoreElements()) {
+			// bg3에 있는 요소를 꺼내서 JRadioButton으로 형변환
+			AbstractButton abstractButton = (AbstractButton) bg3_list.nextElement();
+			JRadioButton jb = (JRadioButton) abstractButton;
+			if (jb.isSelected()) {
+				if (jb == jrb8) {
+					price = price * 0.8;
+				} else if (jb == jrb9) {
+					price = price * 1.0;
+				} else if (jb == jrb10) {
+					price = price * 1.2;
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		new Ex01_Menu();
 	}
+
 }
